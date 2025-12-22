403208405835e47423c8e0f32cd02ea8b8357e5e
Seppo Pakonen <seppo.pakonen@gmail.com>
2025-12-22 06:58:26 +0000

    cleanup

--- build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "Building..."
-echo "Error: Compilation failed" >&2
-exit 1
--- build_error_test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "Error: build error occurred on line 10" >&2
-echo "Some other warning message" >&2
-exit 1
